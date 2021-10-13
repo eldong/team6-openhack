b9f8fb47f16eeccbfbae73b9e97a7e000901aa4a
Joseph Masengesho <jmasengesho@microsoft.com>
2021-10-13 13:08:06 -0500

    some changes

--- IceCreamAPI/Startup.cs
+++ IceCreamAPI/Startup.cs
@@ -2,7 +2,9 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using IceCreamAPI;
-
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+using Azure.Identity;
 
 [assembly: FunctionsStartup(typeof(IceCreamAPI.Startup))]
 namespace IceCreamAPI
@@ -15,12 +17,22 @@ namespace IceCreamAPI
             builder.Services.AddSingleton<IRatingService,RatingService>();
         }
 
+
+
         private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync()
         {
             string databaseName = System.Environment.GetEnvironmentVariable("COSMOS_DB_DATABASE_NAME");
             string containerName = System.Environment.GetEnvironmentVariable("COSMOS_DB_CONTAINER_NAME");
             string account = System.Environment.GetEnvironmentVariable("COSMOS_DB_URL");
             string key = System.Environment.GetEnvironmentVariable("COSMOS_DB_ACCOUNT_KEY");
+
+
+            //var credentials = new DefaultAzureCredential();
+
+
+            // var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            // string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net");
+            // var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
             
             Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
             CosmosDbService cosmosDbService =  new CosmosDbService(client, databaseName, containerName); 
