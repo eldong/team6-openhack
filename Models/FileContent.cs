dc94e34d3078feced057f02b139532cb10ddb0af
Joseph Masengesho <jmasengesho@microsoft.com>
2021-10-14 17:25:27 -0500

    added id

--- Models/FileContent.cs
+++ Models/FileContent.cs
@@ -1,5 +1,11 @@
+using System;
+using Newtonsoft.Json;
+
 public class FileContent
 {
+    [JsonProperty(PropertyName = "batchid")]
     public string BatchId {get;set;}
+    [JsonProperty(PropertyName = "id")]
+    public string Id {get;set;} = Guid.NewGuid().ToString();
     public string Content {get;set;}
 }
